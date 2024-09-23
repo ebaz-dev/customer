4b22d648bcec2ded5b005794c4bcbbdac17f5d4f
tsendebna <tsendbayar.o@ebazaar.mn>
2024-09-23 11:23:57 +0800

    build remove

--- service/build/shared/events/customer-event-subjects.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CustomerEventSubjects = void 0;
-var CustomerEventSubjects;
-(function (CustomerEventSubjects) {
-    CustomerEventSubjects["CustomerCreated"] = "customer:created";
-    CustomerEventSubjects["CustomerUpdated"] = "customer:updated";
-})(CustomerEventSubjects || (exports.CustomerEventSubjects = CustomerEventSubjects = {}));
