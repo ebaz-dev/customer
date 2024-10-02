b056fbee6f0d8cec20cf755709ba9e14778a1c81
tsendebna <tsendbayar.o@ebazaar.mn>
2024-10-02 18:58:57 +0800

    location and category list updated

--- service/src/routes/category-list.ts
+++ service/src/routes/category-list.ts
@@ -1,27 +1,20 @@
 import express, { Request, Response } from "express";
-import { QueryOptions, validateRequest } from "@ebazdev/core";
+import { validateRequest } from "@ebazdev/core";
 import { StatusCodes } from "http-status-codes";
-import { CustomerCategory, customerCategoryRepo } from "../shared";
-import { query } from "express-validator";
+import { CustomerCategory } from "../shared";
 import _ from "lodash";
-
 const router = express.Router();
 
-router.get("/category/list", [query("type").notEmpty().isString().withMessage("Type is required")], validateRequest, async (req: Request, res: Response) => {
-  const criteria: any = { type: req.query.type, parentId: { $exists: false } };
-
-  const options: QueryOptions = <QueryOptions>req.query;
-  options.sortBy = "name";
-  options.sortDir = -1;
-
-  const result = await customerCategoryRepo.selectAndCountAll(criteria, options);
-  const promises = _.map(result.data, async (parent: any, i) => {
-    const subCategories = await customerCategoryRepo.select({ parentId: parent.id });
-    return { ...parent.toJSON(), subCategories }
+router.get("/category/list", validateRequest, async (req: Request, res: Response) => {
+  const criteria: any = { type: req.query.type };
+  const data = await CustomerCategory.find(criteria);
+  const categories = data.filter(category => { return !category.parentId }).map(category => {
+    console.log(category._id)
+    const subCategories = data.filter(subCategory => { return `${subCategory.parentId}` === `${category._id}` })
+    return { id: category.id, parentId: category.parentId, name: category.name, subCategories }
   });
-  const categories = await Promise.all(promises);
-
-  res.status(StatusCodes.OK).send({ data: categories, total: result.total, totalPages: result.totalPages, currentPage: result.currentPage });
+  res.status(StatusCodes.OK).send({ data: categories });
 })
+
 
 export { router as categoryListRouter };
--- service/src/routes/location-list.ts
+++ service/src/routes/location-list.ts
@@ -7,11 +7,15 @@ const router = express.Router();
 
 router.get("/location/list", validateRequest, async (req: Request, res: Response) => {
     const criteria: any = {};
-    if (req.query.parentId) {
-        criteria.parentId = req.query.parentId
-    }
     const data = await Location.find(criteria);
-    res.status(StatusCodes.OK).send({ data, total: (await data).length });
+    const cities: any = data.filter(location => { return location.parentId === 0 }).map((city: any) => {
+        const districts: any = data.filter(district => { return Number(district.parentId) === Number(city.id) }).map((district: any) => {
+            const subDistricts = data.filter(subDistrict => { return Number(subDistrict.parentId) === Number(district.id) })
+            return { id: district.id, parentId: district.parentId, name: district.name, subDistricts }
+        })
+        return { id: city.id, parentId: city.parentId, name: city.name, districts }
+    });
+    res.status(StatusCodes.OK).send({ data: cities });
 })
 
 
